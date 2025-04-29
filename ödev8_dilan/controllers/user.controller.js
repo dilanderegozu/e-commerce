faec2e01e10f865710dbd298c3109fb564fd1068
Dilan <dilanderegizu@icloud.com>
2025-04-29 19:03:40 +0300

    Update user.controller.js

--- ödev8_dilan/controllers/user.controller.js
+++ ödev8_dilan/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const services = require("../services/index");
 const utils = require("../utils/index");
 const { StatusCodes } = require("http-status-codes");
-const baseResponse = require("../dto/baseResponse.dto");
+const baseResponse = require("../dto/baseResponse");
 exports.register = async (req, res) => {
   try {
     const isInvalid = utils.helper.handleValidation(req);
